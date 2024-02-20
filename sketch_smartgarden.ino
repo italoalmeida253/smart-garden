534bf8c3ae8e9bf17ce4577b81065222fd6f3046
Italo Almeida <98362702+italoalmeida253@users.noreply.github.com>
2024-02-20 13:28:21 -0300

    Remove código desnecessário e corrigi erros

--- sketch_smartgarden.ino
+++ sketch_smartgarden.ino
@@ -1,9 +1,8 @@
 #include <TimeLib.h>
 
 int pinoRele = 13;
-int pinoValvula = 12;
 int intervaloEntreCiclos = 6;
-int tempoDeIrrigação = 15;
+int tempoDeIrrigacao = 15;
 int ciclos = 0;
 
 void setup() {
@@ -20,7 +19,7 @@ void loop() {
 
   if (hour() == (intervaloEntreCiclos * ciclos)) {
     digitalWrite(pinoRele, HIGH);
-    delay(tempoDeIrrigação * 1000);
+    delay(tempoDeIrrigacao * 1000);
     digitalWrite(pinoRele, LOW);
     ciclos += 1;
   }
